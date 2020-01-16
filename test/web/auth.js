d879703f908e5527e9f2b78769bc2692b23f3aaa
niftylettuce <niftylettuce@gmail.com>
2020-01-16 16:02:17 -0600

    fix: fixed broken test

--- test/web/auth.js
+++ test/web/auth.js
@@ -11,7 +11,7 @@ test('creates new user', async t => {
       password: '@!#SAL:DMA:SKLM!@'
     }
   });
-  t.is(res.body.redirectTo, '/en/my-account');
+  t.is(res.body.redirectTo, '/en/my-account/domains');
   // Should be 201 for success on create
   t.is(res.status, 200);
 });
