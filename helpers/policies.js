74d9796b9e35c6e5b6b96b21fd362a1aeeb3a187
niftylettuce <niftylettuce@gmail.com>
2020-01-20 04:50:07 -0600

    fix: added loginRoute to policy helper

--- helpers/policies.js
+++ helpers/policies.js
@@ -1,13 +1,19 @@
 const Policies = require('@ladjs/policies');
 
-const { verificationPath, userFields, appName } = require('../config');
+const {
+  passportCallbackOptions,
+  verificationPath,
+  userFields,
+  appName
+} = require('../config');
 const { Users } = require('../app/models');
 
 const policies = new Policies(
   {
     schemeName: appName,
     hasVerifiedEmail: userFields.hasVerifiedEmail,
-    verifyRoute: verificationPath
+    verifyRoute: verificationPath,
+    loginRoute: passportCallbackOptions.failureRedirect
   },
   apiToken => {
     const query = {};
