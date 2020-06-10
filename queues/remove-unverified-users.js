47e0614fb215455f15019e7e378cdcaba84921a8
niftylettuce <niftylettuce@gmail.com>
2020-06-10 05:37:03 -0500

    fix: fixed remove unverified users job

--- queues/remove-unverified-users.js
+++ queues/remove-unverified-users.js
@@ -1,14 +1,31 @@
+const API = require('@ladjs/api');
+const Graceful = require('@ladjs/graceful');
+const Mongoose = require('@ladjs/mongoose');
+const _ = require('lodash');
 const moment = require('moment');
 
-const Users = require('../app/models/user');
+const config = require('../config');
 const logger = require('../helpers/logger');
+
+const Users = require('../app/models/user');
+
+const api = new API({ logger });
+const mongoose = new Mongoose(
+  _.merge({ logger }, api.config.mongoose, config.mongoose)
+);
+
+const graceful = new Graceful({
+  mongooses: [mongoose],
+  logger
+});
 
 //
 // this job removes unverified users from 7d+ ago
 //
 module.exports = async job => {
+  logger.info('starting remove unverified users', { job });
   try {
-    logger.info('starting remove unverified users', { job });
+    await Promise.all([mongoose.connect(), graceful.listen()]);
     await Users.remove({
       github_profile_id: {
         $exists: false
