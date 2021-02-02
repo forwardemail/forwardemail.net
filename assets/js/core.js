c19771596a869ec2a84b7864b91b584523af5531
niftylettuce <niftylettuce@gmail.com>
2021-02-02 11:19:16 -0600

    fix: hide modals when they close per <https://stackoverflow.com/a/66014515/3586413>

--- assets/js/core.js
+++ assets/js/core.js
@@ -218,3 +218,14 @@ $body.on('click', '[data-target="#modal-help"]', (ev) => {
     $('#modal-help').modal('show');
   }
 });
+
+//
+// any modals with embedded <iframe> we can assume need reset
+// <https://stackoverflow.com/a/52315492>
+//
+$body.on('hide.bs.modal', '.modal', function () {
+  const $modal = $(this);
+  if ($modal.find('iframe').length === 0) return;
+  const html = $modal.html();
+  $modal.html(html);
+});
