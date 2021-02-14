6eecb2f33b4c43c72ac5caf6e113e062622bb92b
niftylettuce <niftylettuce@gmail.com>
2021-02-14 00:46:28 -0600

    fix: fixed Lighthouse accessibility score, sync with lad v1.3.0

--- package-scripts.js
+++ package-scripts.js
@@ -25,7 +25,7 @@ module.exports = {
     pretestMongo: "mongo forwardemail_test --eval 'db.dropDatabase()'",
     // <https://stackoverflow.com/a/16974060/3586413>
     pretestRedis:
-      "redis-cli EVAL \"return redis.call('del', 'defaultKey', unpack(redis.call('keys', ARGV[1])))\" 0 limit_test:*",
+      "redis-cli EVAL \"return redis.call('del', 'defaultKey', unpack(redis.call('keys', ARGV[1])))\" 0 *_limit_test:*",
 
     test: 'ava',
     testCoverage: series('nps pretest', 'nyc ava'),
