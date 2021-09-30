8639aaee97533cfa7c638c27d346be6d242be620
Spencer Snyder <sasnyde2@gmail.com>
2021-09-30 16:49:04 -0400

    refactor: separate some my-account functions into files

---
+++ app/controllers/web/my-account/index.js
@@ -0,0 +1,9 @@
+const retrieveDomainBilling = require('./retrieve-domain-billing');
+const updateProfile = require('./update-profile');
+const createDomainBilling = require('./create-domain-billing');
+
+module.exports = {
+  retrieveDomainBilling,
+  createDomainBilling,
+  updateProfile
+};
