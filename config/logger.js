ded130eb98267de10d7ba8b90e439004a11c6849
Taylor Schley <taylorschley@me.com>
2021-10-28 14:22:04 -0600

    feat: add influx logger

--- config/logger.js
+++ config/logger.js
@@ -1,5 +1,6 @@
 const Axe = require('axe');
 const { WebClient } = require('@slack/web-api');
+const { InfluxDB, Point } = require('@influxdata/influxdb-client');
 const signale = require('signale');
 const pino = require('pino')({
   customLevels: {
@@ -102,6 +103,60 @@ if (env.SLACK_API_TOKEN) {
     } catch (err) {
       axe.error(err, { callback: false });
     }
+  });
+}
+
+if (env.INFLUX_API_TOKEN) {
+  // setup influx client
+  const client = new InfluxDB({
+    url: 'http://localhost:8086',
+    token: env.INFLUX_API_TOKEN
+  });
+
+  // org and bucket
+  const org = 'titanism';
+  const bucket = 'forwardemail_logs';
+
+  const writeApi = client.getWriteApi(org, bucket);
+
+  axe.setCallback(async (level, message, meta) => {
+    const point = new Point(level);
+
+    // tags
+    point
+      .tag('hostname', meta.app.hostname)
+      .tag('environment', meta.app.environment)
+      .tag('hash', meta.app.hash);
+
+    if (meta.user && meta.user.email) point.tag('user', meta.user.email);
+
+    if (level === 'error') point.tag('error_code', meta.err.status);
+
+    // request information
+    if (meta.request)
+      point
+        .tag('req_method', meta.request.method)
+        .tag('req_url', meta.request.url)
+        .tag('req_query', JSON.stringify(meta.request.query));
+
+    if (meta.response) point.tag('res_code', meta.response.status_code);
+
+    // fields
+    point.stringField(
+      'message',
+      // message without ascii colors
+      (meta.err && meta.err.message ? meta.err.message : message).replace(
+        /\[\d+m/g,
+        ''
+      )
+    );
+
+    if (meta.err && meta.err.stack) point.stringField('stack', meta.err.stack);
+
+    point.timestamp(new Date());
+
+    writeApi.writePoint(point);
+    axe.info('Logged influx', { callback: false });
   });
 }
 
