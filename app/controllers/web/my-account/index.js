289d34616fd2e43866d0e23708415f8f1762dbeb
Taylor Schley <taylorschley@me.com>
2021-11-18 16:21:10 -0600

    feat: add resend email link

--- app/controllers/web/my-account/index.js
+++ app/controllers/web/my-account/index.js
@@ -4,6 +4,7 @@ const listAliases = require('./list-aliases');
 const listBilling = require('./list-billing');
 const listDomains = require('./list-domains');
 const manageBilling = require('./manage-billing');
+const resendEmailChange = require('./resend-email-change');
 const retrieveBilling = require('./retrieve-billing');
 const retrieveDomainBilling = require('./retrieve-domain-billing');
 const updateProfile = require('./update-profile');
@@ -15,6 +16,7 @@ module.exports = {
   listBilling,
   listDomains,
   manageBilling,
+  resendEmailChange,
   retrieveBilling,
   retrieveDomainBilling,
   updateProfile
