e848f77e9c5afe18f1f944c596f926950674bd2a
Taylor Schley <taylorschley@me.com>
2021-11-20 13:13:15 -0600

    refactor: updateAlias

--- app/controllers/web/my-account.js
+++ app/controllers/web/my-account.js
@@ -68,41 +68,6 @@ const app = new ForwardEmail({
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY);
 
-async function updateAlias(ctx, next) {
-  ctx.state.alias = await Aliases.findById(ctx.state.alias._id);
-  ctx.state.alias = _.extend(ctx.state.alias, ctx.state.body);
-  try {
-    ctx.state.alias.locale = ctx.locale;
-    ctx.state.alias = await ctx.state.alias.save();
-    if (ctx.api) {
-      ctx.state.alias = toObject(Aliases, ctx.state.alias);
-      ctx.state.alias.user = toObject(Users, ctx.state.user);
-      ctx.state.alias.domain = toObject(Domains, ctx.state.domain);
-      ctx.state.alias.domain.members = ctx.state.domain.members;
-      ctx.state.alias.domain.invites = ctx.state.domain.invites;
-      return next();
-    }
-
-    ctx.flash('custom', {
-      title: ctx.request.t('Success'),
-      text: ctx.translate('REQUEST_OK'),
-      type: 'success',
-      toast: true,
-      showConfirmButton: false,
-      timer: 3000,
-      position: 'top'
-    });
-    const redirectTo = ctx.state.l(
-      `/my-account/domains/${ctx.state.domain.name}/aliases`
-    );
-    if (ctx.accepts('html')) ctx.redirect(redirectTo);
-    else ctx.body = { redirectTo };
-  } catch (err) {
-    ctx.logger.error(err);
-    ctx.throw(Boom.badRequest(err.message));
-  }
-}
-
 async function removeAlias(ctx, next) {
   await Aliases.findByIdAndRemove(ctx.state.alias._id);
   if (!ctx.api)
