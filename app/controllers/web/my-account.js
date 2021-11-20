201a28b3bef96a5afed803c4353a98119c9ab60b
Taylor Schley <taylorschley@me.com>
2021-11-20 12:52:47 -0600

    refactor: validateAlias

--- app/controllers/web/my-account.js
+++ app/controllers/web/my-account.js
@@ -68,49 +68,6 @@ const app = new ForwardEmail({
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY);
 
-function validateAlias(ctx, next) {
-  const body = _.pick(ctx.request.body, [
-    'name',
-    'description',
-    'labels',
-    'is_enabled',
-    'recipients'
-  ]);
-
-  if (!isSANB(body.name)) delete body.name;
-
-  if (isSANB(body.description)) body.description = striptags(body.description);
-  else delete body.description;
-
-  if (isSANB(body.labels))
-    body.labels = _.compact(
-      _.uniq(
-        _.map(
-          splitLines(body.labels).join(' ').split(',').join(' ').split(' '),
-          (label) => slug(label)
-        )
-      )
-    );
-  else delete body.labels;
-
-  body.is_enabled = boolean(body.is_enabled);
-
-  if (isSANB(body.recipients))
-    body.recipients = _.compact(
-      _.uniq(
-        _.map(
-          splitLines(body.recipients).join(' ').split(',').join(' ').split(' '),
-          (recipient) => recipient.trim()
-        )
-      )
-    );
-  else delete body.recipients;
-
-  ctx.state.body = body;
-
-  return next();
-}
-
 async function createAlias(ctx, next) {
   try {
     if (isSANB(ctx.state.body.name) && ctx.state.body.name.includes('+'))
