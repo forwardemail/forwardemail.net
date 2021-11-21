05b44b2ddb3a4c2e74ff2479f1b5d487a9c42118
Taylor Schley <taylorschley@me.com>
2021-11-21 12:14:51 -0600

    feat(admin/domains): add update max recipients

--- app/controllers/web/admin/domains.js
+++ app/controllers/web/admin/domains.js
@@ -3,6 +3,10 @@ const paginate = require('koa-ctx-paginate');
 
 const { Domains } = require('../../../models');
 
+const OPTIONS_MAX_RECIPIENTS = [
+  0, 10, 20, 30, 40, 50, 100, 150, 200, 250, 500, 750, 1000
+];
+
 async function list(ctx) {
   const query = {};
 
@@ -26,7 +30,8 @@ async function list(ctx) {
       domains,
       pageCount,
       itemCount,
-      pages: paginate.getArrayPages(ctx)(3, pageCount, ctx.query.page)
+      pages: paginate.getArrayPages(ctx)(3, pageCount, ctx.query.page),
+      OPTIONS_MAX_RECIPIENTS
     });
 
   // this will assign rendered html to ctx.body
@@ -34,7 +39,8 @@ async function list(ctx) {
     domains,
     pageCount,
     itemCount,
-    pages: paginate.getArrayPages(ctx)(3, pageCount, ctx.query.page)
+    pages: paginate.getArrayPages(ctx)(3, pageCount, ctx.query.page),
+    OPTIONS_MAX_RECIPIENTS
   });
 
   const table =
@@ -45,10 +51,39 @@ async function list(ctx) {
   ctx.body = { table };
 }
 
+async function update(ctx) {
+  const domain = await Domains.findById(ctx.params.id);
+
+  if (!domain)
+    throw Boom.notFound(ctx.translateError('DOMAIN_DOES_NOT_EXIST_ANYWHERE'));
+
+  const { body } = ctx.request;
+
+  // save max recipients or if null keep the same
+  domain.max_recipients_per_alias =
+    body.max_recipients_per_alias || domain.max_recipients_per_alias;
+
+  await domain.save();
+
+  ctx.flash('custom', {
+    title: ctx.request.t('Success'),
+    text: ctx.translate('REQUEST_OK'),
+    type: 'success',
+    toast: true,
+    showConfirmButton: false,
+    timer: 3000,
+    position: 'top'
+  });
+
+  if (ctx.accepts('html')) ctx.redirect('back');
+  else ctx.body = { reloadPage: true };
+}
+
 async function remove(ctx) {
   const domain = await Domains.findById(ctx.params.id);
 
-  if (!domain) throw Boom.notFound(ctx.translateError('INVALID_USER'));
+  if (!domain)
+    throw Boom.notFound(ctx.translateError('DOMAIN_DOES_NOT_EXIST_ANYWHERE'));
 
   await domain.remove();
   ctx.flash('custom', {
@@ -67,5 +102,6 @@ async function remove(ctx) {
 
 module.exports = {
   list,
-  remove
+  remove,
+  update
 };
