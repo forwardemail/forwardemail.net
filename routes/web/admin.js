5b8aa88455de15f76e8b1345b8cda1bdd74ce880
Taylor Schley <taylorschley@me.com>
2021-11-21 11:24:10 -0600

    feat(admin/domains): list domains

--- routes/web/admin.js
+++ routes/web/admin.js
@@ -11,10 +11,15 @@ router.use(policies.ensureAdmin);
 router.use(policies.ensureOtp);
 router.use(web.breadcrumbs);
 router.get('/', render('admin'));
+
+// users
 router.get('/users', paginate.middleware(10, 50), web.admin.users.list);
 router.get('/users/:id', web.admin.users.retrieve);
 router.put('/users/:id', web.admin.users.update);
 router.post('/users/:id/login', web.admin.users.login);
 router.delete('/users/:id', web.admin.users.remove);
+
+// domains
+router.get('/domains', paginate.middleware(10, 50), web.admin.domains.list);
 
 module.exports = router;
