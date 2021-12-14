c7e251af6885540b925ce9e9731c58a98f4ae947
Taylor Schley <taylorschley@me.com>
2021-12-14 09:40:04 -0600

    refactor: removeAlias

---
+++ app/controllers/web/my-account/remove-alias.js
@@ -0,0 +1,23 @@
+const { Aliases } = require('../../models');
+
+async function removeAlias(ctx, next) {
+  await Aliases.findByIdAndRemove(ctx.state.alias._id);
+  if (!ctx.api)
+    ctx.flash('custom', {
+      title: ctx.request.t('Success'),
+      text: ctx.translate('REQUEST_OK'),
+      type: 'success',
+      toast: true,
+      showConfirmButton: false,
+      timer: 3000,
+      position: 'top'
+    });
+  if (ctx.api) return next();
+  const redirectTo = ctx.state.l(
+    `/my-account/domains/${ctx.state.domain.name}/aliases`
+  );
+  if (ctx.accepts('html')) ctx.redirect(redirectTo);
+  else ctx.body = { redirectTo };
+}
+
+module.exports = removeAlias;
