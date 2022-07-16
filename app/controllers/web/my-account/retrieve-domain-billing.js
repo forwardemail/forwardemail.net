001935eea0925b2513df463e17431651a319f22b
titanism <101466223+titanism@users.noreply.github.com>
2022-07-16 00:16:01 -0500

    feat: started stripe webhook integration, use toast notifications for past due message (less obtrusive), enabled zoom/toolbar on admin dashboard charts, ensured pdf receipt uses buffer, added job to fix non-free accounts, auto-downgrade to free if non-free account condition met, rate limited pdf downloads

--- app/controllers/web/my-account/retrieve-domain-billing.js
+++ app/controllers/web/my-account/retrieve-domain-billing.js
@@ -979,6 +979,37 @@ async function retrieveDomainBilling(ctx) {
       domain.locale = ctx.locale;
       domain.client = ctx.client;
       ctx.state.domain = await domain.save();
+      //
+      // NOTE: this logic is the same as `jobs/fix-non-free-users`
+      //       (which runs every minute as plans expire, but this is to ensure its real-time)
+      //
+      // downgrade the user if past due,
+      // not on free plan, zero domains on paid plans,
+      // and no subscription
+      if (
+        ctx.state.user.plan !== 'free' &&
+        new Date(ctx.state.user[config.userFields.planExpiresAt]).getTime() <
+          Date.now() &&
+        !isSANB(ctx.state.user[config.userFields.stripeSubscriptionID]) &&
+        !isSANB(ctx.state.user[config.userFields.paypalSubscriptionID])
+      ) {
+        const count = await Domains.countDocuments({
+          members: {
+            $elemMatch: {
+              user: ctx.state.user._id,
+              group: 'admin'
+            }
+          },
+          plan: { $ne: 'free' }
+        });
+        if (count === 0) {
+          ctx.logger.info(`updating to free plan`);
+          ctx.state.user.plan = 'free';
+          ctx.state.user[config.userFields.planSetAt] = new Date();
+          ctx.flash('success', ctx.translate('FREE_PLAN'));
+          await ctx.state.user.save();
+        }
+      }
     }
 
     // if ctx.query.plan was free and it was an account billing change
