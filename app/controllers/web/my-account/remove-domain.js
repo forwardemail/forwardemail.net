88615160578a41e61d54a830a17c72dfb987b89f
titanism <101466223+titanism@users.noreply.github.com>
2022-07-16 00:28:06 -0500

    fix: ensure domain removal will downgrade appropriately

--- app/controllers/web/my-account/remove-domain.js
+++ app/controllers/web/my-account/remove-domain.js
@@ -1,4 +1,7 @@
+const isSANB = require('is-string-and-not-blank');
+
 const { Domains, Aliases } = require('#models');
+const config = require('#config');
 
 async function removeDomain(ctx, next) {
   // remove all aliases
@@ -17,6 +20,39 @@ async function removeDomain(ctx, next) {
       timer: 3000,
       position: 'top'
     });
+
+  //
+  // NOTE: this logic is the same as `jobs/fix-non-free-users`
+  //       (which runs every minute as plans expire, but this is to ensure its real-time)
+  //
+  // downgrade the user if past due,
+  // not on free plan, zero domains on paid plans,
+  // and no subscription
+  if (
+    ctx.state.user.plan !== 'free' &&
+    new Date(ctx.state.user[config.userFields.planExpiresAt]).getTime() <
+      Date.now() &&
+    !isSANB(ctx.state.user[config.userFields.stripeSubscriptionID]) &&
+    !isSANB(ctx.state.user[config.userFields.paypalSubscriptionID])
+  ) {
+    const count = await Domains.countDocuments({
+      members: {
+        $elemMatch: {
+          user: ctx.state.user._id,
+          group: 'admin'
+        }
+      },
+      plan: { $ne: 'free' }
+    });
+    if (count === 0) {
+      ctx.logger.info(`updating to free plan`);
+      ctx.state.user.plan = 'free';
+      ctx.state.user[config.userFields.planSetAt] = new Date();
+      if (!ctx.api) ctx.flash('success', ctx.translate('FREE_PLAN'));
+      await ctx.state.user.save();
+    }
+  }
+
   if (ctx.api) return next();
   const redirectTo = ctx.state.l('/my-account/domains');
   if (ctx.accepts('html')) ctx.redirect(redirectTo);
