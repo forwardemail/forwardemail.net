d9bcd42bfe9da8dc97ea8e8c40be4146adfefdb8
titanism <101466223+titanism@users.noreply.github.com>
2022-07-26 14:01:41 -0500

    feat: added one-off job to clear UCEPROTECT if needed

---
+++ jobs/clear-uceprotect.js
@@ -0,0 +1,45 @@
+// eslint-disable-next-line import/no-unassigned-import
+require('#config/env');
+
+const process = require('process');
+const { parentPort } = require('worker_threads');
+
+const Graceful = require('@ladjs/graceful');
+const Redis = require('@ladjs/redis');
+const sharedConfig = require('@ladjs/shared-config');
+const validator = require('validator');
+
+const logger = require('#helpers/logger');
+
+const breeSharedConfig = sharedConfig('BREE');
+const client = new Redis(breeSharedConfig.redis, logger);
+const graceful = new Graceful({
+  redisClients: [client],
+  logger
+});
+
+graceful.listen();
+
+(async () => {
+  try {
+    // delete all IP's prefixed with backscatter or blacklist
+    // (this will ensure our latest dataset is accurate)
+    const [blacklistKeys, backscatterKeys] = await Promise.all([
+      client.keys('blacklist:*'),
+      client.keys('backscatter:*')
+    ]);
+    const pipeline = client.pipeline();
+    for (const key of [...blacklistKeys, ...backscatterKeys]) {
+      // filter out keys to be IP addresses only
+      const [, ip] = key.split(':');
+      if (validator.isIP(ip)) pipeline.del(key);
+    }
+
+    await pipeline.exec();
+  } catch (err) {
+    logger.fatal(err);
+  }
+
+  if (parentPort) parentPort.postMessage('done');
+  else process.exit(0);
+})();
