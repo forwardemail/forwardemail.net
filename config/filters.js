567758f2f7f2a025719d91eacb65d9e4f3735c0b
titanism <101466223+titanism@users.noreply.github.com>
2023-01-11 13:37:54 -0600

    fix: fixed svg and collapse on h3, added min-vh-80 across the site

--- config/filters.js
+++ config/filters.js
@@ -84,15 +84,12 @@ function fixTableOfContents(content, i18n, options) {
   for (const header of root.querySelectorAll('h1,h2,h3,h4,h5,h6')) {
     const anchor = header.querySelector('a');
     if (!anchor) continue;
-    let id = anchor.getAttribute('id');
-    if (!id && anchor.getAttribute('href'))
-      id = anchor.getAttribute('href').slice(1);
 
-    if (!id) continue;
+    const id = header.getAttribute('id');
 
     anchor.setAttribute('class', 'anchor');
 
-    if (lis.length > 5) {
+    if (lis.length > 5 && header.rawTagName === 'h2') {
       // eslint-disable-next-line unicorn/prefer-dom-node-dataset
       anchor.setAttribute('data-toggle', 'collapse');
       anchor.setAttribute('role', 'button');
@@ -149,7 +146,30 @@ function fixTableOfContents(content, i18n, options) {
         node.nextElementSibling.rawTagName !== 'h2'
       ) {
         node = node.nextElementSibling;
-        html += node.toString();
+
+        const parsedNode = parse(node);
+
+        //
+        // iterate over the node's children h3, h4, h5, h6
+        // and if they are linked then rewrite them with svg appropriately
+        //
+        for (const header of parsedNode.querySelectorAll('h3,h4,h5,h6')) {
+          // this is the same code as above
+          const anchor = header.querySelector('a');
+
+          // eslint-disable-next-line max-depth
+          if (!anchor) continue;
+          const id = header.getAttribute('id');
+          // eslint-disable-next-line max-depth
+          if (!id) continue;
+          anchor.setAttribute('class', 'anchor');
+          anchor.removeAttribute('aria-hidden');
+          anchor.set_content(
+            '<svg aria-hidden="true" class="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>'
+          );
+        }
+
+        html += parsedNode.toString();
         nodes.push(node);
       }
 
@@ -260,6 +280,10 @@ const i18n = new I18N({
   syncFiles: false,
   logger
 });
+
+//
+// TODO: create an in-memory hash map with rev-hash to render quick in production
+//
 
 module.exports = {
   md(string, options) {
