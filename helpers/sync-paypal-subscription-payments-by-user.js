552fd94a69dcfb60c3c9d0022eebe75376d8983c
titanism <101466223+titanism@users.noreply.github.com>
2023-01-24 14:16:43 -0600

    fix: updated paypal sync job and script

---
+++ helpers/sync-paypal-subscription-payments-by-user.js
@@ -0,0 +1,350 @@
+const dayjs = require('dayjs-with-plugins');
+const isSANB = require('is-string-and-not-blank');
+const ms = require('ms');
+const pMapSeries = require('p-map-series');
+const parseErr = require('parse-err');
+
+const Payments = require('#models/payment');
+const Users = require('#models/user');
+const config = require('#config');
+const emailHelper = require('#helpers/email');
+const logger = require('#helpers/logger');
+const { paypalAgent } = require('#helpers/paypal');
+
+const { PAYPAL_PLAN_MAPPING } = config.payments;
+const PAYPAL_PLANS = {
+  enhanced_protection: Object.values(PAYPAL_PLAN_MAPPING.enhanced_protection),
+  team: Object.values(PAYPAL_PLAN_MAPPING.team)
+};
+const thresholdError = new Error('Error threshold has been met');
+
+async function syncPayPalSubscriptionPaymentsByUser(
+  customer,
+  errorEmails = []
+) {
+  try {
+    logger.info(`Syncing paypal subscription payments for ${customer.email}`);
+    // first we need to get the distinct paypal order Ids and validate them all
+    // this really shouldn't be needed. So I am leaving it out for now unless
+    // we want to specifically validate these in the future
+    //
+    // const orderIds = await Payments.distinct('paypal_order_id', {
+    //   user: customer._id
+    // });
+    //
+    // for (const orderId of orderIds) {
+    //   const agent = await paypalAgent();
+    //   const { body: order } = await agent.get(
+    //     `/v2/checkout/orders/${orderId}`
+    //   );
+    //   ;
+    // }
+
+    // then we need to get all the subscription ids and validate that the one that
+    // works is the subscription id set on the user. Assuming that is good, that will
+    // be the only subscription we have access to I think...
+    // This kind of sucks, but it is the best we can do right now I beleive.
+    const subscriptionIds = await Payments.distinct(
+      config.userFields.paypalSubscriptionID,
+      {
+        user: customer._id
+      }
+    );
+
+    // push the user's subscription ID if it was set but not included
+    if (
+      isSANB(customer[config.userFields.paypalSubscriptionID]) &&
+      !subscriptionIds.includes(
+        customer[config.userFields.paypalSubscriptionID]
+      )
+    )
+      subscriptionIds.push(customer[config.userFields.paypalSubscriptionID]);
+
+    // eslint-disable-next-line no-inner-declarations
+    async function subscriptionMapper(subscriptionId) {
+      let hasError = false;
+      logger.info(`subscriptionId ${subscriptionId}`);
+      try {
+        const agent = await paypalAgent();
+        const { body: subscription } = await agent.get(
+          `/v1/billing/subscriptions/${subscriptionId}`
+        );
+
+        const plan = Object.keys(PAYPAL_PLANS).find((plan) =>
+          PAYPAL_PLANS[plan].includes(subscription.plan_id)
+        );
+
+        const duration = ms(
+          Object.entries(PAYPAL_PLAN_MAPPING[plan]).find(
+            (mapping) => mapping[1] === subscription.plan_id
+          )[0]
+        );
+
+        //
+        // NOTE: this re-uses the auth token from `agent` created above
+        // (so there should not be more than 30s delay between the two calls)
+        //
+        // this will either error - or it will return the current active subscriptions transactions.
+        // https://developer.paypal.com/docs/subscriptions/full-integration/subscription-management/#list-transactions-for-a-subscription
+        const { body: { transactions } = {} } = await agent.get(
+          `/v1/billing/subscriptions/${subscriptionId}/transactions?start_time=${
+            subscription.create_time
+          }&end_time=${new Date().toISOString()}`
+        );
+
+        if (Array.isArray(transactions) && transactions.length > 0) {
+          logger.info(`${transactions.length} transactions`);
+
+          // eslint-disable-next-line no-inner-declarations
+          async function transactionMapper(transaction) {
+            try {
+              // we need to have a payment for each transaction of a subscription
+              logger.info(`transaction ${transaction.id}`);
+
+              // TODO: there is def an issue here with mismatch
+              // try to find the payment
+              const paymentCandidates = await Payments.find({
+                user: customer._id,
+                [config.userFields.paypalSubscriptionID]: subscription.id
+              });
+
+              // then use it if its on the same day
+              const payment = paymentCandidates.find(
+                (p) =>
+                  transaction.id === p.paypal_transaction_id ||
+                  dayjs(transaction.time).format('MM/DD/YY') ===
+                    dayjs(p.invoice_at).format('MM/DD/YY')
+              );
+
+              if (
+                isSANB(
+                  transaction.amount_with_breakdown.gross_amount.currency_code
+                ) &&
+                transaction.amount_with_breakdown.gross_amount.currency_code !==
+                  'USD'
+              )
+                throw new Error(
+                  'Paypal transaction amount was not in USD and could not be saved by sync-payment-histories'
+                );
+
+              const amount =
+                Number.parseInt(
+                  transaction.amount_with_breakdown.gross_amount.value,
+                  10
+                ) * 100;
+
+              let amountRefunded = 0;
+              // if the transaction was refunded or partially
+              // refunded then we need to check and update it
+              if (transaction.status === 'REFUNDED') {
+                amountRefunded = amount;
+              } else if (transaction.status === 'PARTIALLY_REFUNDED') {
+                // lookup the refund and parse the amount refunded
+                const agent = await paypalAgent();
+                const { body: refund } = await agent.get(
+                  `/v2/payments/refunds/${transaction.id}`
+                );
+                amountRefunded = Math.round(Number(refund.amount.value) * 100);
+              }
+
+              if (payment) {
+                let shouldSave = false;
+
+                if (!payment.paypal_transaction_id) {
+                  // prevent double tx id save
+                  const count = await Payments.countDocuments({
+                    paypal_transaction_id: transaction.id,
+                    _id: {
+                      $ne: payment._id
+                    }
+                  });
+
+                  if (count > 0)
+                    throw new Error(
+                      `Capture ID ${transaction.id} was attempting to be duplicated for payment ID ${payment.id}`
+                    );
+
+                  // otherwise set the tx id
+                  payment.paypal_transaction_id = transaction.id;
+                  shouldSave = true;
+                }
+
+                // transaction time is different than invoice_at, which is used for plan expiry calculation
+                // (see jobs/fix-missing-invoice-at.js)
+                if (
+                  new Date(payment.invoice_at).getTime() !==
+                  new Date(transaction.time).getTime()
+                ) {
+                  // if the payment's invoice_at was not equal to transaction time
+                  payment.invoice_at = new Date(transaction.time);
+                  shouldSave = true;
+                }
+
+                if (payment.plan !== plan)
+                  throw new Error('Paypal plan did not match');
+
+                if (payment.amount_refunded !== amountRefunded) {
+                  payment.amount_refunded = amountRefunded;
+                  shouldSave = true;
+                }
+
+                if (payment.duration !== duration) {
+                  payment.duration = duration;
+                  shouldSave = true;
+                }
+
+                if (shouldSave) {
+                  logger.info(`Updating existing payment ${payment.id}`);
+                  await payment.save();
+                } else {
+                  logger.info(
+                    `payment ${payment.id} already up to date and good to go!`
+                  );
+                }
+              } else {
+                // prevent double tx id save
+                const count = await Payments.countDocuments({
+                  paypal_transaction_id: transaction.id
+                });
+
+                if (count > 0)
+                  throw new Error(
+                    `Capture ID ${transaction.id} was attempting to be duplicated for customer ${customer.email}`
+                  );
+
+                // otherwise set the tx id
+                const payment = {
+                  user: customer._id,
+                  method: 'paypal',
+                  kind: 'subscription',
+                  amount,
+                  plan,
+                  duration,
+                  amount_refunded: amountRefunded,
+                  [config.userFields.paypalSubscriptionID]: subscription.id,
+                  paypal_transaction_id: transaction.id,
+                  invoice_at: new Date(transaction.time)
+                };
+                logger.info('creating new payment');
+                await Payments.create(payment);
+              }
+
+              // find and save the associated user
+              // so that their plan_expires_at gets updated
+              const user = await Users.findById(customer._id);
+              if (!user) throw new Error('User does not exist');
+              await user.save();
+            } catch (err) {
+              logger.error(err);
+              hasError = true;
+              errorEmails.push({
+                template: 'alert',
+                message: {
+                  to: config.email.message.from,
+                  subject: `${customer.email} had an issue syncing a transaction from paypal subscription ${subscriptionId} and transaction ${transaction.id}`
+                },
+                locals: {
+                  message: `<pre><code>${JSON.stringify(
+                    parseErr(err),
+                    null,
+                    2
+                  )}</code></pre>`
+                }
+              });
+
+              if (errorEmails.length >= config.paypalErrorThreshold)
+                throw thresholdError;
+            }
+          }
+
+          await pMapSeries(transactions, transactionMapper);
+        }
+
+        // after we have finished syncing subscriptions
+        // if the subscription itself was cancelled
+        // then we need to remove it from our system
+        if (
+          !hasError &&
+          isSANB(subscription.status) &&
+          ['SUSPENDED', 'CANCELLED', 'EXPIRED'].includes(subscription.status)
+        ) {
+          // attempt to cancel the subscription completely (if status was not "CANCELLED" explicitly)
+          if (subscription.status !== 'CANCELLED') {
+            try {
+              const agent = await paypalAgent();
+              await agent.post(
+                `/v1/billing/subscriptions/${subscriptionId}/cancel`
+              );
+            } catch (err) {
+              logger.error(err, { customer });
+            }
+          }
+
+          // remove it from the user's account
+          // (if and only if the subscription ID matched and was current)
+          const user = await Users.findById(customer._id);
+          if (!user) throw new Error('User does not exist');
+          if (user[config.userFields.paypalSubscriptionID] === subscriptionId) {
+            user[config.userFields.paypalSubscriptionID] = undefined;
+            await user.save();
+          }
+        }
+      } catch (err) {
+        logger.error(err);
+
+        if (err === thresholdError) throw err;
+
+        if (err.status === 404)
+          logger.fatal(new Error('paypal subscription does not exist'), {
+            customer
+          });
+        else {
+          errorEmails.push({
+            template: 'alert',
+            message: {
+              to: config.email.message.from,
+              subject: `${customer.email} has an issue syncing all payments from paypal subscription ${subscriptionId} that were not synced by the sync-payment-histories job`
+            },
+            locals: {
+              message: `<pre><code>${JSON.stringify(
+                parseErr(err),
+                null,
+                2
+              )}</code></pre>`
+            }
+          });
+
+          if (errorEmails.length >= config.paypalErrorThreshold)
+            throw thresholdError;
+        }
+      }
+    }
+
+    await pMapSeries(subscriptionIds, subscriptionMapper);
+  } catch (err) {
+    logger.error(err, { customer });
+    if (err === thresholdError) {
+      try {
+        await emailHelper({
+          template: 'alert',
+          message: {
+            to: config.email.message.from,
+            subject: `Sync PayPal payment histories hit ${config.paypalErrorThreshold} errors during the script`
+          },
+          locals: {
+            message:
+              'This may have occurred because of an error in the script, or the paypal service was down, or another error was causing an abnormal number of payment syncing failures'
+          }
+        });
+      } catch (err) {
+        logger.error(err);
+      }
+
+      throw err;
+    }
+  }
+
+  return errorEmails;
+}
+
+module.exports = syncPayPalSubscriptionPaymentsByUser;
--- scripts/set-active-pypl-subscription-ids.js
+++ scripts/set-active-pypl-subscription-ids.js
@@ -9,11 +9,12 @@ const Mongoose = require('@ladjs/mongoose');
 const sharedConfig = require('@ladjs/shared-config');
 const pMap = require('p-map');
 
-const config = require('#config');
-const logger = require('#helpers/logger');
-const { paypalAgent } = require('#helpers/paypal');
-
 const Users = require('#models/user');
+const config = require('#config');
+const emailHelper = require('#helpers/email');
+const logger = require('#helpers/logger');
+const syncPayPalSubscriptionPaymentsByUser = require('#helpers/sync-paypal-subscription-payments-by-user');
+const { paypalAgent } = require('#helpers/paypal');
 
 const breeSharedConfig = sharedConfig('BREE');
 
@@ -23,6 +24,12 @@ const graceful = new Graceful({
   mongooses: [mongoose],
   logger
 });
+
+const { PAYPAL_PLAN_MAPPING } = config.payments;
+const PAYPAL_PLANS = {
+  enhanced_protection: Object.values(PAYPAL_PLAN_MAPPING.enhanced_protection),
+  team: Object.values(PAYPAL_PLAN_MAPPING.team)
+};
 
 graceful.listen();
 
@@ -34,22 +41,27 @@ async function mapper(id) {
   });
   if (count > 1) console.log(`count was > 1 for ${id}`);
   if (count === 1) return;
-  console.log(`${id} was missing a user`);
+
+  console.log(`${id} was missing a user, performing lookups now`);
+
   // lookup the active subscription details
   const agent = await paypalAgent();
   const { body: subscription } = await agent.get(
     `/v1/billing/subscriptions/${id}`
   );
+
   // lookup the user by parsed subscription information
   let user = await Users.findOne({
     [config.userFields.paypalPayerID]: subscription.subscriber.payer_id,
     [config.userFields.paypalSubscriptionID]: { $exists: false }
   });
+
   if (!user)
     user = await Users.findOne({
       email: subscription.subscriber.email_address.toLowerCase(),
       [config.userFields.paypalSubscriptionID]: { $exists: false }
     });
+
   if (!user) {
     console.log('-------------------------------------------------------');
     console.log(`User could not be found for subscription ID ${id}`);
@@ -59,15 +71,37 @@ async function mapper(id) {
   }
 
   console.log(`found user ${user.email} for ${id}`);
+
   // save the subscription ID to the profile
   user[config.userFields.paypalSubscriptionID] = id;
+
+  if (user.plan === 'free') {
+    const plan = Object.keys(PAYPAL_PLANS).find((plan) =>
+      PAYPAL_PLANS[plan].includes(subscription.plan_id)
+    );
+    console.log(`setting user plan from ${user.plan} to ${plan}`);
+    user.plan = plan;
+  }
+
   await user.save();
-  console.log('saved user');
+
+  // create and sync payments
+  const errorEmails = await syncPayPalSubscriptionPaymentsByUser(user);
+
+  if (errorEmails.length > 0) {
+    try {
+      await Promise.all(errorEmails.map((email) => emailHelper(email)));
+    } catch (err) {
+      logger.error(err);
+    }
+  }
+
+  // TODO: we also need to do this for stripe and then also put both this in webhook
 }
 
-const ids = [
-  // TODO: paste in here from report export
-];
+// TODO: paste in here from report export
+// <https://gist.github.com/titanism/955f0c21d53e8c98068c549fb79e75d4>
+const ids = [];
 
 (async () => {
   await mongoose.connect();
