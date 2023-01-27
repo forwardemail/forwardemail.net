72cbb3066e922c82079b4c2edfb1e41b29dca1e7
titanism <101466223+titanism@users.noreply.github.com>
2023-01-26 21:16:15 -0600

    feat: update stripe job, sync locales

--- jobs/stripe/check-duplicate-subscriptions.js
+++ jobs/stripe/check-duplicate-subscriptions.js
@@ -2,15 +2,16 @@ const os = require('os');
 
 const Stripe = require('stripe');
 const pMap = require('p-map');
+const pMapSeries = require('p-map-series');
+const _ = require('lodash');
 
 const getAllStripeCustomers = require('./get-all-stripe-customers');
-const getAllStripePaymentIntents = require('./get-all-stripe-payment-intents');
 
 const env = require('#config/env');
 const config = require('#config');
 const logger = require('#helpers/logger');
 const Users = require('#models/user');
-const Payments = require('#models/payment');
+const emailHelper = require('#helpers/email');
 
 const concurrency = os.cpus().length;
 const stripe = new Stripe(env.STRIPE_SECRET_KEY);
@@ -26,32 +27,22 @@ async function mapper(customer) {
   // check for user on our side
   const user = await Users.findOne({
     [config.userFields.stripeCustomerID]: customer.id
-  });
-  if (!user) {
-    console.log(`User does not exist for customer ID ${customer.id}`);
-    return;
-  }
+  })
+    .lean()
+    .exec();
+
+  if (!user) return;
 
   // if emails did not match
-  if (user.email !== customer.email)
-    console.log(
+  if (user.email !== customer.email) {
+    logger.info(
       `User email ${user.email} did not match customer email ${customer.email} (${customer.id})`
     );
-
-  // check for payments by customer and ensure that all payments exist on our side
-  const paymentIntents = await getAllStripePaymentIntents(customer.id);
-
-  // ensure that all payment intents exist
-  const count = await Payments.countDocuments({
-    stripe_payment_intent_id: {
-      $in: paymentIntents.map((pi) => pi.id)
-    }
-  });
-
-  if (count !== paymentIntents.length)
-    console.log(
-      `User had missing payments ${user.email} (${customer.id}) (count: ${count}) (payment intents: ${paymentIntents.length})`
-    );
+    customer = await stripe.customers.update(customer.id, {
+      email: user.email
+    });
+    logger.info(`Updated user email to match ${user.email}`);
+  }
 
   // check for active subscriptions
   // (if there is more than one than error)
@@ -67,23 +58,77 @@ async function mapper(customer) {
     })
   ]);
 
-  const subscriptions = [...activeSubscriptions, ...trialingSubscriptions];
+  if (activeSubscriptions.has_more || trialingSubscriptions.has_more)
+    throw new Error(
+      'Subscriptions has_more bug - this should not have pagination'
+    );
+
+  let subscriptions = [
+    ...activeSubscriptions.data,
+    ...trialingSubscriptions.data
+  ];
 
   if (subscriptions.length > 1) {
-    console.log(
-      `User had multiple subscriptions ${user.email} (${customer.id})`
+    logger.fatal(
+      new Error(
+        `We may need to refund: User had multiple subscriptions ${user.email} (${customer.id})`
+      )
     );
+    await emailHelper({
+      template: 'alert',
+      message: {
+        to: config.email.message.from,
+        subject: `Customer had multiple subscriptions: ${user.email}`
+      },
+      locals: {
+        message: `<p>Customer may have had duplicate subscription payments and may need some of them refunded.</p>
+          <p>Subscriptions are already synced correctly to the latest trial or most recently active.</p>
+          <p><a href="https://dashboard.stripe.com/customers/${customer.id}" class="btn btn-dark btn-lg" target="_blank" rel="noopener noreferrer">Review Stripe Customer</a></p>`
+      }
+    });
+
+    // find the subscription that is trialing
+    // and cancel all the other ones
+    // otherwise if no trialing then cancel everything but newest
+    // save to the user the trialing or newest
+    const trialing = subscriptions.find((s) => s.status === 'trialing');
+    if (trialing) {
+      await Users.findByIdAndUpdate(user._id, {
+        $set: {
+          [config.userFields.stripeSubscriptionID]: trialing.id
+        }
+      });
+      const subscriptionsToCancel = subscriptions.filter(
+        (s) => s.id !== trialing.id
+      );
+      await pMapSeries(subscriptionsToCancel, async (subscription) => {
+        await stripe.subscriptions.del(subscription.id);
+      });
+    } else {
+      // sort subscriptions by `created` in reverse (gets newest timestamp first)
+      subscriptions = _.sortBy(subscriptions, 'created').reverse();
+      const [first, ...others] = subscriptions;
+      await Users.findByIdAndUpdate(user._id, {
+        $set: {
+          [config.userFields.stripeSubscriptionID]: first.id
+        }
+      });
+      await pMapSeries(others, async (subscription) => {
+        await stripe.subscriptions.del(subscription.id);
+      });
+    }
   } else if (
-    !user[config.userFields.stripeSubscriptionID] ||
-    user[config.userFields.stripeSubscriptionID] !== subscriptions[0].id
+    subscriptions.length === 1 &&
+    (!user[config.userFields.stripeSubscriptionID] ||
+      user[config.userFields.stripeSubscriptionID] !== subscriptions[0].id)
   ) {
     // if subscription ID was not saved to the user then error
-    console.log(
-      `User subscription did not match ${user.email} (${customer.id})`
-    );
+    await Users.findByIdAndUpdate(user._id, {
+      $set: {
+        [config.userFields.stripeSubscriptionID]: subscriptions[0].id
+      }
+    });
   }
-
-  // TODO: send email here
 }
 
 async function checkDuplicateSubscriptions() {
