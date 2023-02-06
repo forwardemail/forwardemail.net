e7f2cecb3600b085fe22c7e8e947b1125fc74771
titanism <101466223+titanism@users.noreply.github.com>
2023-02-06 00:45:59 -0600

    chore: fixed script to cleanup pypl data

--- scripts/check-dup-payer-id.js
+++ scripts/check-dup-payer-id.js
@@ -15,22 +15,24 @@ const pReduce = require('p-reduce');
 const mongoose = require('mongoose');
 const { Users, Payments } = require('#models');
 const config = require('#config');
-const logger = require('#helpers/logger');
 const emailHelper = require('#helpers/email');
 const setupMongoose = require('#helpers/setup-mongoose');
 const syncPayPalSubscriptionPaymentsByUser = require('#helpers/sync-paypal-subscription-payments-by-user');
 
 const graceful = new Graceful({
-  mongooses: [mongoose],
-  logger
+  mongooses: [mongoose]
 });
 
 graceful.listen();
 
 async function mapper(id) {
   // get all users with this paypal payer id
+  // and active paypal subscription
   const users = await Users.find({
-    [config.userFields.paypalPayerID]: id
+    [config.userFields.paypalPayerID]: id,
+    [config.userFields.paypalSubscriptionID]: {
+      $exists: true
+    }
   })
     .lean()
     .exec();
@@ -47,7 +49,11 @@ async function mapper(id) {
     }
   });
 
+  console.log(`unique subscription ids`);
+  console.log(payments.map((p) => p[config.userFields.paypalSubscriptionID]));
+
   // delete all of those payments and then sync paypal subscription payments by user
+  // NOTE: this only works because the user has a current subscription id
   console.log(
     `deleting ${payments.length} payments for ${users.length} users for payer ID ${id}`
   );
@@ -64,16 +70,36 @@ async function mapper(id) {
     try {
       await Promise.all(errorEmails.map((email) => emailHelper(email)));
     } catch (err) {
-      logger.error(err);
+      console.error(err);
     }
   }
 }
 
 (async () => {
-  await setupMongoose(logger);
+  await setupMongoose();
+  await pMapSeries(subscriptionIds, async (id) => {
+    const distinctUsers = await Payments.distinct('user', {
+      [config.userFields.paypalSubscriptionID]: id
+    });
+    if (distinctUsers.length > 1)
+      throw new Error(
+        `paypal subscription id ${id} had payments with different user ids`
+      );
+  });
+
   const ids = await Users.distinct(config.userFields.paypalPayerID, {
     [config.userFields.paypalPayerID]: { $exists: true }
   });
   await pMapSeries(ids, mapper);
+
+  // check for payments that have the same paypal_subscription_id but different user
+  const subscriptionIds = await Payments.distinct(
+    config.userFields.paypalSubscriptionID,
+    {
+      [config.userFields.paypalSubscriptionID]: {
+        $exists: true
+      }
+    }
+  );
   process.exit(0);
 })();
