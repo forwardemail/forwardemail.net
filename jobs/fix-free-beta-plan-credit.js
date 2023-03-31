b5acab76c85a2827c54c292bb4549121f3a0ba44
titanism <101466223+titanism@users.noreply.github.com>
2023-03-31 15:42:28 -0500

    fix: reward users with free credit per outage on 3/22

--- jobs/fix-free-beta-plan-credit.js
+++ jobs/fix-free-beta-plan-credit.js
@@ -32,22 +32,22 @@ async function mapper(id) {
   const user = await Users.findById(id);
   if (!user) throw new Error('User does not exist');
   // check if the user had zero payments
-  const count = await Payments.countDocuments({ user: user._id });
-  if (count > 0) return;
+  // const count = await Payments.countDocuments({ user: user._id });
+  // if (count > 0) return;
   // create a new free beta program credit for them
   await Payments.create({
     user: user._id,
     amount:
       user.plan === 'enhanced_protection'
-        ? Math.round(300 * 12)
-        : Math.round(900 * 12),
+        ? Math.round(300 * 3)
+        : Math.round(900 * 3),
     amount_refunded:
       user.plan === 'enhanced_protection'
-        ? Math.round(300 * 12)
-        : Math.round(900 * 12),
+        ? Math.round(300 * 3)
+        : Math.round(900 * 3),
     invoice_at: dayjs().startOf('day').toDate(),
     method: 'free_beta_program',
-    duration: ms('1y'),
+    duration: ms('90d'), // 3 months
     plan: user.plan,
     kind: 'one-time'
   });
@@ -63,9 +63,14 @@ async function mapper(id) {
     // and are not banned and have verified emails
     const ids = await Users.distinct('_id', {
       plan: { $ne: 'free' },
+      created_at: {
+        $lte: dayjs('3/22/23', 'M/DD/YY').endOf('day').toDate()
+      },
       [config.userFields.hasVerifiedEmail]: true,
       [config.userFields.isBanned]: false
     });
+
+    console.log('ids.length', ids.length);
 
     await pMap(ids, mapper, { concurrency });
   } catch (err) {
