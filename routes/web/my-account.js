f7758926995364821a085e338917b0590e6dda67
titanism <101466223+titanism@users.noreply.github.com>
2023-05-18 12:32:52 -0500

    fix: increased rate limit on downloading receipts from 1 to 3 yrs

--- routes/web/my-account.js
+++ routes/web/my-account.js
@@ -96,10 +96,10 @@ router
     rateLimit(100, 'create domain billing'),
     web.myAccount.createDomainBilling
   )
-  // rate limit 1 year of receipts due to PDF downloading
+  // rate limit to 3 years of receipts due to PDF downloading
   .get(
     '/billing/:reference',
-    rateLimit(12, 'retrieve receipt'),
+    rateLimit(36, 'retrieve receipt'),
     web.myAccount.retrieveReceipt
   )
   .get('/domains', paginate.middleware(10, 50), web.myAccount.listDomains)
