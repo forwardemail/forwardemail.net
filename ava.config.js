abd5443fde2d90bab0550f32c6ae22637f235e62
titanism <101466223+titanism@users.noreply.github.com>
2023-06-06 19:21:08 -0500

    fix: added worker threads fix for sharp/ava per <https://github.com/lovell/sharp/issues/3164>

--- ava.config.js
+++ ava.config.js
@@ -1,6 +1,10 @@
+const { familySync, GLIBC } = require('detect-libc');
+
 module.exports = {
   verbose: true,
   failFast: true,
   serial: true,
-  files: ['test/*.js', 'test/**/*.js', 'test/**/**/*.js', '!test/utils.js']
+  files: ['test/*.js', 'test/**/*.js', 'test/**/**/*.js', '!test/utils.js'],
+  // <https://github.com/lovell/sharp/issues/3164#issuecomment-1168328811>
+  workerThreads: familySync() !== GLIBC
 };
