bbdb65334b4db8077405bedac5c6d157cc61c156
titanism <101466223+titanism@users.noreply.github.com>
2023-06-06 19:32:22 -0500

    fix: increase ava default timeout for CI

--- ava.config.js
+++ ava.config.js
@@ -1,3 +1,4 @@
+const isCI = require('is-ci');
 const { familySync, GLIBC } = require('detect-libc');
 
 module.exports = {
@@ -6,5 +7,6 @@ module.exports = {
   serial: true,
   files: ['test/*.js', 'test/**/*.js', 'test/**/**/*.js', '!test/utils.js'],
   // <https://github.com/lovell/sharp/issues/3164#issuecomment-1168328811>
-  workerThreads: familySync() !== GLIBC
+  workerThreads: familySync() !== GLIBC,
+  timeout: isCI ? '1m' : '15s'
 };
