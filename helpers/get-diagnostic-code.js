17fcb7e5c5ecd3714390f4d4e628b1cb713bcaf2
titanism <101466223+titanism@users.noreply.github.com>
2023-08-30 15:10:41 -0500

    fix: safari perf optimizations, strip html from diagnostic

--- helpers/get-diagnostic-code.js
+++ helpers/get-diagnostic-code.js
@@ -1,14 +1,29 @@
 const RE2 = require('re2');
 const isSANB = require('is-string-and-not-blank');
+const { convert } = require('html-to-text');
 
 const getErrorCode = require('./get-error-code');
 
+const config = require('#config');
+
 const REGEX_DIAGNOSTIC_CODE = new RE2(/^\d{3} /);
+
+const options = {
+  wordwrap: false,
+  selectors: [
+    { selector: 'img', format: 'skip' },
+    { selector: 'ul', options: { itemPrefix: ' ' } },
+    {
+      selector: 'a',
+      options: { baseUrl: config.urls.web, linkBrackets: false }
+    }
+  ]
+};
 
 function getDiagnosticCode(err) {
   if (isSANB(err.response) && REGEX_DIAGNOSTIC_CODE.test(err.response))
-    return err.response;
-  return `${getErrorCode(err)} ${err.message}`;
+    return convert(err.response, options);
+  return `${getErrorCode(err)} ${convert(err.message, options)}`;
 }
 
 module.exports = getDiagnosticCode;
