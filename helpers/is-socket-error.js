748b21304ec5224d14ab5b98e7dc0e51ad96b72f
titanism <101466223+titanism@users.noreply.github.com>
2023-09-26 17:04:45 -0500

    fix: added pmx false per <https://github.com/Unitech/pm2/issues/4510\#issuecomment-1736359090>, added connection pool closed error check

--- helpers/is-socket-error.js
+++ helpers/is-socket-error.js
@@ -1,6 +1,7 @@
 function isSocketError(err) {
   return Boolean(
     err.message === 'Connection closed' ||
+      err.message === 'Connection pool was closed' ||
       err.message === 'Connection closed unexpectedly' ||
       err.message === 'Socket closed unexpectedly' ||
       err.message === 'Unexpected socket close' ||
