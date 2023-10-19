d4c0d5bd28728d0ea3c7854c8e893456ecfb14a2
titanism <101466223+titanism@users.noreply.github.com>
2023-10-19 15:12:38 -0500

    fix: fixed license in old file

--- test/imap/index.js
+++ test/imap/index.js
@@ -1,13 +1,17 @@
-//
-// NOTE: This file is licensed under the GPLv3 license, see `IMAP-LICENSE`.
-//
-//       Source code was created with inspiration from WildDuck at <https://github.com/nodemailer/wildduck>.
-//       WildDuck Mail Agent is licensed under the European Union Public License 1.2 or later.
-//       <https://github.com/nodemailer/wildduck/blob/master/LICENSE>
-//
-//       Thank you to Andris Reinman, the author of WildDuck, Nodemailer, and many other OSS projects.
-//       Please consider sponsoring their efforts as we do on GitHub at <https://github.com/andris9>.
-//
+/*
+ * Copyright (c) Forward Email LLC
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *   WildDuck Mail Agent is licensed under the European Union Public License 1.2 or later.
+ *   https://github.com/nodemailer/wildduck
+ */
 
 const { Buffer } = require('node:buffer');
 const { createHash } = require('node:crypto');
