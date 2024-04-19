2d81f40c065523e8ad7f5a4a4de327a6b6d7f696
titanism <101466223+titanism@users.noreply.github.com>
2024-04-19 07:50:41 -0500

    fix: arbitrary comment/code to trigger cache change

--- assets/js/core.js
+++ assets/js/core.js
@@ -25,6 +25,9 @@ function toArrayBuffer(buffer) {
   );
 }
 
+// arbitrary comment to trigger re-cache
+(() => {})();
+
 // load jQuery and Bootstrap
 // <https://stackoverflow.com/a/34340392>
 // <https://github.com/FezVrasta/popper.js/issues/287#issuecomment-321887784>
