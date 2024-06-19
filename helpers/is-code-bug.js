b387f279090932b1201d59f88a6cef41c1957abf
titanism <101466223+titanism@users.noreply.github.com>
2024-06-18 19:20:56 -0500

    fix: added more isCodeBug err.name values for detection

--- helpers/is-code-bug.js
+++ helpers/is-code-bug.js
@@ -18,6 +18,8 @@ function isCodeBug(err) {
         err.message === CONNECTION_CLOSED_ERROR_MSG) ||
       err.name === 'SqliteError' ||
       err.code === 'SQLITE_ERROR' ||
+      err.name === 'MongooseServerSelectionError' ||
+      err.name === 'MongoNetworkError' ||
       // <https://github.com/WiseLibs/better-sqlite3/blob/007d43e229190618884a9f976909c0b14a17d82c/docs/api.md?plain=1#L611>
       (typeof err.code === 'string' &&
         err.code.startsWith('UNKNOWN_SQLITE_ERROR_')) ||
@@ -34,6 +36,8 @@ function isCodeBug(err) {
       (isErrorConstructorName(err, 'MongooseError') &&
         !isErrorConstructorName(err, 'ValidationError')) ||
       isErrorConstructorName(err, 'MongoError') ||
+      isErrorConstructorName(err, 'MongoNetworkError') ||
+      isErrorConstructorName(err, 'MongooseServerSelectionError') ||
       isErrorConstructorName(err, 'RedisError')
   );
   // safeguard
