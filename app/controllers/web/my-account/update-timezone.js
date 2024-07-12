f4a252e526178a9217e7aa7a759fd02800dc7f7e
titanism <101466223+titanism@users.noreply.github.com>
2024-07-11 22:53:14 -0500

    fix: timezone should not trigger hooks but purely a mongo update

--- app/controllers/web/my-account/update-timezone.js
+++ app/controllers/web/my-account/update-timezone.js
@@ -6,12 +6,17 @@
 const Boom = require('@hapi/boom');
 const isSANB = require('is-string-and-not-blank');
 
+const Users = require('#models/users');
+
 async function updateTimezone(ctx) {
   if (!isSANB(ctx.request.body.timeZone))
     throw Boom.badRequest(ctx.translateError('UNKNOWN_ERROR'));
 
-  ctx.state.user.timezone = ctx.request.body.timeZone;
-  ctx.state.user = await ctx.state.user.save();
+  await Users.findByIdAndUpdate(ctx.state.user._id, {
+    $set: {
+      timezone: ctx.request.body.timeZone
+    }
+  });
 
   if (ctx.accepts('html')) ctx.redirect('back');
   else ctx.body = { reloadPage: true };
