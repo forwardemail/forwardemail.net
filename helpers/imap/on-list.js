8b89ca343f111a28eacc77e8436726301ddd4cb8
titanism <101466223+titanism@users.noreply.github.com>
2024-07-14 17:38:10 -0500

    fix: fixed onList not handling err like other methods

--- helpers/imap/on-list.js
+++ helpers/imap/on-list.js
@@ -28,9 +28,7 @@ async function onList(query, session, fn) {
       mailboxes.map((m) => m.toObject())
     );
   } catch (err) {
-    const error = refineAndLogError(err, session, true, this);
-    if (error.imapResponse) return fn(null, error.imapResponse);
-    fn(error);
+    fn(refineAndLogError(err, session, true, this));
   }
 }
 
