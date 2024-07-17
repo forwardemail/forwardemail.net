a9f37d2ae552c4fe3e4ac3440446c3629625b111
titanism <101466223+titanism@users.noreply.github.com>
2024-07-17 02:07:34 -0500

    fix: more sqlite fixes

--- sqlite-server.js
+++ sqlite-server.js
@@ -44,7 +44,7 @@ class SQLite {
     // worker pool threads
     this.piscina = new Piscina({
       filename: path.resolve(__dirname, 'helpers', 'worker.js'),
-      maxQueue: 4 // rudimentary limit, could also be "auto"
+      maxQueue: 1 // rudimentary limit, could also be "auto"
     });
 
     // start server with either http or https
