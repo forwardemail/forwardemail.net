22d5c0429e75e0f5c0b5f8be75257601020157e7
titanism <101466223+titanism@users.noreply.github.com>
2024-07-19 14:07:09 -0500

    fix: test sharp@latest per <https://github.com/lovell/sharp/issues/3164\#issuecomment-2239933471>

--- ava.config.js
+++ ava.config.js
@@ -4,7 +4,7 @@
  */
 
 const isCI = require('is-ci');
-const { familySync, GLIBC } = require('detect-libc');
+// const { familySync, GLIBC } = require('detect-libc');
 
 module.exports = {
   verbose: true,
@@ -12,6 +12,6 @@ module.exports = {
   serial: true,
   files: ['test/*.js', 'test/**/*.js', 'test/**/**/*.js', '!test/utils.js'],
   // <https://github.com/lovell/sharp/issues/3164#issuecomment-1168328811>
-  workerThreads: familySync() !== GLIBC,
+  // workerThreads: familySync() !== GLIBC,
   timeout: isCI ? '3m' : '1.5m'
 };
