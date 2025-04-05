d4ce59e1ab7a23acca4c414c79bda515b32acb39
Shaun Warman <shaunwarman1@gmail.com>
2025-04-05 10:43:12 -0500

    fix: update ava for ci

--- ava.config.js
+++ ava.config.js
@@ -3,17 +3,17 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-// const os = require('node:os');
+const os = require('node:os');
 
-// const isCI = require('is-ci');
+const isCI = require('is-ci');
 
 // const { familySync, GLIBC } = require('detect-libc');
 
 module.exports = {
   verbose: true,
   failFast: true,
-  // serial: true,
-  concurrency: 2, // isCI ? 2 : Math.floor(os.cpus().length / 3),
+  serial: isCI,
+  concurrency: isCI ? 2 : Math.floor(os.cpus().length / 3),
   files: ['test/*.js', 'test/**/*.js', 'test/**/**/*.js', '!test/utils.js'],
   // <https://github.com/lovell/sharp/issues/3164#issuecomment-1168328811>
   // workerThreads: familySync() !== GLIBC,
