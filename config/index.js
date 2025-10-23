59d3067e288b565874dcc78d431650e8eb9a9b60
titanism <101466223+titanism@users.noreply.github.com>
2025-10-23 13:30:36 -0500

    fix: fixed imap tests

--- config/index.js
+++ config/index.js
@@ -693,7 +693,7 @@ const config = {
     'SQLITE_BUSY_TIMEOUT',
     'SQLITE_LOCKED'
   ]),
-  INITIAL_DB_SIZE: 270336,
+  INITIAL_DB_SIZE: 282624,
   STRIPE_LOCALES,
   openPGPKey: '/.well-known/openpgpkey/hu/mxqp8ogw4jfq83a58pn1wy1ccc1cx3f5.asc',
   returnPath: 'fe-bounces',
