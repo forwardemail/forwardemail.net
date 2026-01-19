f30ff738032da1923a8f84706133b6f464cfb705
titanism <101466223+titanism@users.noreply.github.com>
2026-01-19 01:44:03 -0600

    chore: initial db size increase

--- config/index.js
+++ config/index.js
@@ -694,7 +694,7 @@ const config = {
     'SQLITE_BUSY_TIMEOUT',
     'SQLITE_LOCKED'
   ]),
-  INITIAL_DB_SIZE: 290816,
+  INITIAL_DB_SIZE: 294912,
   STRIPE_LOCALES,
   openPGPKey: '/.well-known/openpgpkey/hu/mxqp8ogw4jfq83a58pn1wy1ccc1cx3f5.asc',
   returnPath: 'fe-bounces',
